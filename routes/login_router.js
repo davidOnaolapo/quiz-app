ec21ab177d59aaa0230909096f57d349d493e225
David Onaolapo <talk2dawich@gmail.com>
2021-07-28 17:20:31 +0000

    authenticate user, test post route

--- routes/login_router.js
+++ routes/login_router.js
@@ -4,26 +4,27 @@ const router  = express.Router();
 const { authenticate, generateRandomString } = require("../lib/helpers.js");
 const userQueries = require('../db/queries/user_queries')
 
-// router.get('/', (req, res) => {
-//   userQueries.getUsers()
-//     .then((users) => {
-//       res.json(users);
-//     })
-//     .catch((e) => {
-//       console.log(e.message);
-//     })
-// });
-
 router.get('/', (req, res) => {
-  // if (!req.body.text) {
-  //   res.status(400).json({ error: 'invalid request: no data in POST body'});
-  //   return;
-  // }
+  userQueries.getUsers()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((e) => {
+      console.log(e.message);
+    })
+});
+
+router.post('/', (req, res) => {
+  if (!req.body) {
+    res.status(400).json({ error: 'invalid request: no data in POST body'});
+    return;
+  }
 
   authenticate({email: 'alice_wonderland@gmail.com', password: 'alice'}, false)
     .then((yo) => {
-      console.log("FInished authentication")
-      res.json(yo)
+      console.log(req.body["quiz-question"]);
+      // console.log(req.session.username);
+      res.redirect('/quiz_wiz')
     })
     .catch((e) => {
       console.log(e.message);
