3fd8c1f9d97a59e68c430e77fa392a77b0bf21ad
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-07-29 11:43:56 +0000

    update event handlers

--- public/scripts/event-handlers.js
+++ public/scripts/event-handlers.js
@@ -5,24 +5,26 @@ $(document).ready(function() {
   })
 
 
-  $("form").submit(function(event) {     //event handler for new quizzes
-    event.preventDefault();
-
-    const $serializedData = $(this).serialize();
-    const $formText = $(this.text);
-
-    $.post('/create_quiz', $serializedData)
-    .then(function(data) {
-      loadQuizzes();
-      $(".new-quiz").slideUp("fast");
-      $($formText).val('');
-      console.log($serializedData);
-
-    })
-  })
 
 
 });
+
+
+$("form").submit(function(event) {     //event handler for new quizzes
+  event.preventDefault();
+
+  const $serializedData = $(this).serialize();
+  const $formText = $(this.text);
+
+  $.post('/create_quiz', $serializedData)
+  .then(function(data) {
+    loadQuizzes();
+    $(".new-quiz").slideUp("fast");
+    $($formText).val('');
+    console.log($serializedData);
+
+  })
+})
 
 
 function submit_quiz(event) {     //event handler for quiz submissions
